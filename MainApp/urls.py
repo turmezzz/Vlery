9f2f5ce0771a724794f872ae6bb6ea3832a2247b
Turmets Makoev <tzmakoev@edu.hse.ru>
2018-05-05 16:13:10 +0300

    upd

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
+    url(r'^logout', views.logout, name='logout'),
     url(r'^get_access_token', views.get_access_token, name='get_access_token'),
     url(r'^search', views.search, name='search'),
 
