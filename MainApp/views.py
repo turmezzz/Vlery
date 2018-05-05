dde797314ebe0193bdab492d74c1cc1d28dfc778
Turmets Makoev <tzmakoev@edu.hse.ru>
2018-05-05 23:55:32 +0300

    upd

--- MainApp/views.py
+++ MainApp/views.py
@@ -15,8 +15,8 @@ from . import tools
 def get_access_token(request):
     client_id = '6340308'
     client_secret = 'K1umBhHtCQNdl6LW8bVk'
-    # redirect_uri = 'http://turmezzz.pythonanywhere.com/get_access_token'
-    redirect_uri = 'blank.html'
+    redirect_uri = 'http://turmezzz.pythonanywhere.com/get_access_token'
+    # redirect_uri = 'blank.html'
     code = request.GET['code']
     url = f'https://oauth.vk.com/access_token?client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&code={code}'
     data = json.loads(urllib.request.urlopen(url).read())
