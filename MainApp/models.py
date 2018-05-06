ed347867f75fb6e26e6245172d7e24fa5fc37db5
Turmets Makoev <tzmakoev@edu.hse.ru>
2018-05-07 02:22:30 +0300

    upd

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,11 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # vk_id is username
-    # access_token = models.CharField(max_length=100, default='')
-
-    def __init__(self):
-        self.username = models.CharField(max_length=100, default='')
-        self.access_token = models.CharField(max_length=100, default='')
+    access_token = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.username
