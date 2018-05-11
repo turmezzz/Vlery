48b2c9b87ead856ed4fe5bdb042331d51e3186f6
Turmets Makoev <tzmakoev@edu.hse.ru>
2018-05-12 01:48:42 +0300

    upd

--- MainApp/tools.py
+++ MainApp/tools.py
@@ -125,7 +125,7 @@ def search(user, q):
     q_words = q.split()
     data = {}
     for word in q_words:
-        posts = Post.objects.filter(owner_id__exact=owner_id, text__contains=word)
+        posts = Post.objects.filter(owner_id__exact=owner_id, text__contains=(' ' + word + ' '))
         for post in posts:
             if post not in data:
                 data[post] = 0
